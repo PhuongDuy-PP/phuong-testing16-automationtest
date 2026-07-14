f456778d3d17bdceb1a73725610e0e7837003550
DuyPhuong <15520659@gm.uit.edu.vn>
2026-07-14 19:39:13 +0700

    Implement handle testing with many browser

--- testing16-selenium-pom/src/test/java/tests/BaseTest.java
+++ testing16-selenium-pom/src/test/java/tests/BaseTest.java
@@ -5,9 +5,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import utils.DriverFactory;
 
+import java.lang.reflect.Method;
 import java.time.Duration;
 
 
@@ -24,24 +29,37 @@ public class BaseTest {
     }
 
 //    setup moi truong
+    @Parameters({"browser", "device"})
     @BeforeMethod(alwaysRun = true)
-    public void setUp() {
+    public void setUp(@Optional("chrome") String browser, @Optional("") String device, Method method) {
+//        TODO: tạo folder chứa screenshot, video record
+
+
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
 
-        WebDriver driver = new ChromeDriver(options);
+
+//        WebDriver driver = new ChromeDriver(options);
+        WebDriver driver = DriverFactory.createDriver(browser, device);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
         driverThreadLocal.set(driver);
         waitThreadLocal.set(wait);
+
+//        TODO: start recording
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+//        CHỈ LƯU RECORD VỚI TEST CASE LỖI
+//        VỚI TEST CASE PASS => CLEAR RECORD ĐÓ
 
         WebDriver driver = getDriver();
+
+//        TODO: kiểm tra test case pass hay fail để xem xét lưu record
+
         if(driver != null){
             driver.quit();
         }
